4e2977880166fa7a8ac0b6c6d838fbff6c25612a
DexDevNL2023 <dexternlang@gmail.com>
2024-10-08 02:34:42 -0700

    dex5500

--- src/main/java/com/teleo/manager/generic/utils/GenericUtils.java
+++ src/main/java/com/teleo/manager/generic/utils/GenericUtils.java
@@ -78,7 +78,7 @@ public class GenericUtils {
         boolean isProd = "prod".equalsIgnoreCase(activeProfile);
         if (isProd) {
             scheme = "https";
-            serverName = "samba-web-api-c9e839abd962.herokuapp.com/";
+            serverName = "samba-web-api-c9e839abd962.herokuapp.com";
         }
 
         // Construire l'URL de base avec le schéma et le nom du serveur
